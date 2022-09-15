b256fdabe71b6452c3df7057722751130202ca1a
SpeedrunDi <amanmaksat0@gmail.com>
2022-09-15 19:38:10 +0600

    initial commit

---
+++ serverApi/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongo: {
+    db: 'mongodb://localhost/todoList',
+    options: {useNewUrlParser: true}
+  }
+};
---
+++ serverApi/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const exitHook = require('async-exit-hook');
+const config = require('./config');
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+
+const run = async () => {
+  await mongoose.connect(config.mongo.db, config.mongo.options);
+
+  app.listen(port, () => {
+    console.log(`Server started on ${port} port`);
+  });
+
+  exitHook(() => {
+    mongoose.disconnect();
+    console.log('MongoDb disconnect');
+  });
+};
+
+run().catch(e => console.error(e));
